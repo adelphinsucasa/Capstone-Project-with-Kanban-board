8e3a3bae322a4573b66bfdc69a69d6ca7ace7cad
haftamudesta <desta451616@hotmail.com>
2023-02-15 17:53:18 +0300

    Add comment popup

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -2,6 +2,10 @@ import fetchCharacter from './apiManagement.js';
 import { setLike, getLike } from './involmentApi.js';
 
 const ul = document.querySelector('.cardContainer');
+const commentSection = document.querySelector('.comment__section');
+const main = document.querySelector('.cardCharacters');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
 
 const renderData = async (index) => {
   const cardIni = (index - 1) * 13 + 1;
@@ -45,29 +49,65 @@ const renderData = async (index) => {
   getLike().then((likes) => {
     let i = 1;
     characteres.forEach((character) => {
-      if ((i >= cardIni) && (i <= cardEnd)) {
-        const itemFound = likes.find((item) => item.item_id === `img${character.id}`);
-        const numLikes = (itemFound !== undefined) ? itemFound.likes : 0;
-        const imgUrl = (itemFound !== undefined) ? './assets/images/icons-sparkling-heart-48.png' : './assets/images/icons-white-heart-48.png';
+      if (i >= cardIni && i <= cardEnd) {
+        const itemFound = likes.find(
+          (item) => item.item_id === `img${character.id}`
+        );
+        const numLikes = itemFound !== undefined ? itemFound.likes : 0;
+        const imgUrl =
+          itemFound !== undefined
+            ? './assets/images/icons-sparkling-heart-48.png'
+            : './assets/images/icons-white-heart-48.png';
         ul.innerHTML += `<li class="card">
-                                        <div class="imageCard">
-                                            <img src="${character.imageUrl}" alt="Character Image">
-                                        </div>
-                                        <div class="cardData">
-                                            <div class="nameTitle">
+          <div class="imageCard">
+      <img src="${character.imageUrl}" alt="Character Image">
+</div>
+ <div class="cardData">
+<div class="nameTitle">
                                                 <h2 class="cardName">${character.fullName}</h2>
                                                 <h3 class="cardName">${character.title}</h3>
-                                            </div>
-                                            <div class="likes">
+</div>
+<div class="likes">
                                                 <img id="img${character.id}" class="imageLike" src="${imgUrl}" alt="">
                                                 <p>${numLikes} likes</p>
-                                            </div>
-                                        </div>
-                                        <div class="cardButtons">
-                                            <button class="btnComment">comments</button>
-                                            <button class="btnReservation">reservation</button>
-                                        </div>
-                                </li>`;
+  </div>
+</div>
+<div class="cardButtons">
+<button class="btnComment">comments</button>
+<button class="btnReservation">reservation</button>
+</div>
+</li>`;
+        const comment = document.querySelectorAll('.btnComment');
+        comment.forEach((element) => {
+          element.addEventListener('click', (e) => {
+            commentSection.innerHTML = '';
+            e.preventDefault();
+            commentSection.classList.add('active');
+            main.classList.add('hide');
+            header.classList.add('hide');
+            footer.classList.add('hide');
+            console.log('clicked');
+            commentSection.innerHTML = `<div class="comment__section-close">
+              <h4>x</h4>
+            </div>
+            <div class="comment__section-detail">
+            <img src="${character.imageUrl}" alt="" />
+            <p>id:${character.id}</p>
+            <p>first name:${character.firstName}</p>
+            <p>last name:${character.fullName}</p>
+            <p>family:${character.family}</p>
+              <p>full name:${character.fullName}</p>
+              <p>title:${character.title}</p>
+            </div>
+            <div class="comment__section-input">
+              <h2 class="add-comment">Add comment</h2>
+              <input type="text" class="name" placeholder="Your Name" />
+              <input type="text" class="comment" placeholder="add comment here" />
+              <button class="btnSubmitt">submit</button>
+            </div>`;
+          });
+        });
+
         const imgLike = document.querySelectorAll('.imageLike');
 
         imgLike.forEach((item) => {
