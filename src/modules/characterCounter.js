db56db4669fb303182c3678cb1a6d4b55fd8fcf4
haftamudesta <desta451616@hotmail.com>
2023-02-17 04:56:15 +0300

    modify module

--- src/modules/characterCounter.js
+++
@@ -1,9 +0,0 @@
-import { fetchCharacter } from './apiManagement.js';
-
-const totalCharacter = document.querySelector('.counter');
-const countCharacter = async () => {
-  const counter = await fetchCharacter();
-  totalCharacter.innerHTML = `(${counter.length})`;
-  return counter.length;
-};
-export default countCharacter;
