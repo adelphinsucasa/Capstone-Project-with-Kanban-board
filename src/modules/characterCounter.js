a25a7447ff937d43f4d2dab3f22539826e98c165
haftamudesta <desta451616@hotmail.com>
2023-02-17 05:02:51 +0300

    Modify module

--- src/modules/characterCounter.js
+++ src/modules/characterCounter.js
@@ -4,6 +4,7 @@ const totalCharacter = document.querySelector('.counter');
 const countCharacter = async () => {
   const counter = await fetchCharacter();
   totalCharacter.innerHTML = `(${counter.length})`;
+  console.log(counter)
   return counter.length;
 };
 export default countCharacter;
