b9b5eebad207005c65365e73c172660bf459a87f
haftamudesta <desta451616@hotmail.com>
2023-02-17 06:48:04 +0300

    Modify character counter

--- src/modules/characterCounter.js
+++ src/modules/characterCounter.js
@@ -2,7 +2,8 @@ import { fetchCharacter } from './apiManagement.js';
 
 const totalCharacter = document.querySelector('.counter');
 const countCharacter = async () => {
-  const counter = await fetchCharacter();
+  const counter = await console.log(counter);
+  fetchCharacter();
   totalCharacter.innerHTML = `(${counter.length})`;
   return counter.length;
 };
